225a6b0658ff36c40247daed4bb66dc4d464d5eb
Aminu Abubakar <aminukano585@gmail.com>
2022-12-18 06:33:13 -0800

    fix hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Print: A string with printf
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
