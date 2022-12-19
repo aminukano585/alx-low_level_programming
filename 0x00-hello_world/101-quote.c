0e3f5878b8896914ee5a839bd0c4002b932aecac
Aminu Abubakar <aminukano585@gmail.com>
2022-12-19 02:00:04 -0800

    print to stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Print: A string to the stderr
+ *
+ * Return: Always 1
+ */
+int main(void)
+{
+	fprintf(stderr, "%s", "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}
