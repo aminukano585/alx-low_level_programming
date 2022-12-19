c66cf1e3918f03f7bb397e592de61772c1b987f2
Aminu Abubakar <aminukano585@gmail.com>
2022-12-19 02:10:47 -0800

    fix betty linter warning

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,8 @@
  */
 int main(void)
 {
-	fprintf(stderr, "%s", "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fprintf(stderr, "%s", "and that piece of art is useful\" - \
+			Dora Korpar, 2015-10-19\n");
 
 	return (1);
 }
