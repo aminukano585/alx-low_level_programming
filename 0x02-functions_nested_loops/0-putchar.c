655b9a419db1fc8efe1e7e3f455962f3cf623ac7
Aminu Abubakar <aminukano585@gmail.com>
2022-12-19 21:17:45 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Print: characters in a string
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char str[] = "_putchar";
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+	return (0);
+}
