019d68d64955fa61033301c3ec0f7be06a6413f1
Aminu Abubakar <aminukano585@gmail.com>
2023-01-01 11:54:47 -0800

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * times_table - function that prints 9 times table
+ * Return: void
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			int k = i * j;
+
+			if (j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+
+			if (k >= 0 && k < 10)
+			{
+				_putchar(' ');
+				_putchar('0' + k);
+			}
+			else
+			{
+				_putchar('0' + k / 10);
+				_putchar('0' + k % 10);
+			}
+		}
+
+		_putchar('\n');
+	}
+}
