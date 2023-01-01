a806f805adab767c0e9c083bca8314aaf2abc6c6
Aminu Abubakar <aminukano585@gmail.com>
2023-01-01 12:24:30 -0800

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - function that adds two integers and returns the result
+ * @p: first parameter
+ * @q: second parameter
+ * Return: int result of adding p and q
+ */
+int add(int p, int q)
+{
+	int result;
+
+	result = p + q;
+
+	return (result);
+}
