0c446f83c4decf58722af7f745906c3398b80add
Aminu Abubakar <aminukano585@gmail.com>
2023-01-17 23:15:07 -0800

    print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print numbers 0 to 9 (except 2 and 4)
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i == 2 || i == 4)
+			continue;
+
+		_putchar(i + '0');
+	}
+
+	_putchar('\n');
+}
